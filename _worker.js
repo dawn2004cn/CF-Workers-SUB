20b2236878311a0b95e8855d1b05178b05bee130
dawn2004cn <dawn2004cn@163.com>
2025-06-20 22:47:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,8 @@ https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
 
 let urls = [];
 let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+//let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConfig = "https://raw.githubusercontent.com/liandu2024/clash/refs/heads/main/proxy/clash-all.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
